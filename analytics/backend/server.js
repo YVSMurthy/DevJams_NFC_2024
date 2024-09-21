c64313c01477de86cf7d6ae692e72f040e31fce2
Y V Suyash Murthy <suyash.murthy@gmail.com>
2024-09-21 15:11:30 +0530

    server dummy data sent

--- analytics/backend/server.js
+++ analytics/backend/server.js
@@ -4,9 +4,38 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("This is the default home page");
+})
+
+app.get('/getPieDataGender', (req, res) => {
+    console.log("Request receied")
+    try {
+        res.status(200).send({data: [57,34], ok: true})
+    }
+    catch {
+        res.status(500).send({message: "Some internal error occured", ok: false})
+    }
+})
+
+app.get('/getPieDataCustomerType', (req, res) => {
+    try {
+        res.status(200).send({data: [47,53], ok: true})
+    }
+    catch {
+        res.status(500).send({message: "Some internal error occured", ok: false})
+    }
+})
+
+app.get('getLineData', (req, res) => {
+    try {
+        res.status(200).send({data: [34, 40, 57, 50, 65], ok: true})
+    }
+    catch {
+        res.status(500).send({message: "Some internal error occured", ok: false})
+    }
 })
 
 app.listen(3001);
