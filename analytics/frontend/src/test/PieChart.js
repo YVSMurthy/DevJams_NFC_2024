a09f0e8468e89a1dd31350a7bdda933a5327eb4e
AabhasGaur19 <aabhasgaur14@gmai8l.com>
2024-09-21 18:17:57 +0530

    data from backend and filtering

--- analytics/frontend/src/test/PieChart.js
+++ analytics/frontend/src/test/PieChart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 
-const PieChart = () => {
+export const PieChart = ({ chartData }) => {
   const chartRef = useRef(null);
   const chartInstance = useRef(null);
 
@@ -9,41 +9,46 @@ const PieChart = () => {
     if (chartInstance.current) {
       chartInstance.current.destroy();
     }
-    const myChartRef = chartRef.current.getContext('2d');
+    const labels = chartData?.labels;
+    const data = chartData?.data;
 
-    chartInstance.current = new Chart(myChartRef, {
-      type: 'pie',
-      data: {
-        labels: ['Label 1', 'Label 2', 'Label 3'],
-        datasets: [
-          {
-            data: [300, 50, 100],
-            backgroundColor: [
-              'rgb(255, 99, 132)',
-              'rgb(54, 162, 235)',
-              'rgb(255, 205, 86)',
-            ],
-          },
-        ],
-      },
-      options: {
-        responsive: true,
-        maintainAspectRatio: true,
-      },
-    });
+    console.log(labels)
+    console.log(data)
 
+    if (labels && data) {
+      const myChartRef = chartRef.current.getContext('2d');
+      console.log('Rendering chart with data:', labels, data);
+
+      chartInstance.current = new Chart(myChartRef, {
+        type: 'pie',
+        data: {
+          labels: labels, 
+          datasets: [
+            {
+              data: data.data, 
+              backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)'
+              ],
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: true,
+        },
+      });
+    }
     return () => {
       if (chartInstance.current) {
         chartInstance.current.destroy();
       }
     };
-  }, []);
+  }, [chartData]);
 
   return (
     <div className="w-[100%] h-[48%] p-4 border rounded-lg border-gray-300 flex items-center justify-center">
-      <canvas ref={chartRef} className="h-[40%] w-auto" />
+      <canvas ref={chartRef} className="h-[35%] w-auto" />
     </div>
   );
 };
-
-export default PieChart;
